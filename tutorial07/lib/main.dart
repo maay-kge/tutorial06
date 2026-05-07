4a885d2de0df7abc6f85e78424c0894059926769
ds <ds@labdc.etec>
2026-05-06 22:08:48 -0300

    primeiro commit

---
+++ tutorial07/lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:tutorial07/database.dart';
+import 'package:tutorial07/model/filme_model.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatefulWidget {
+  const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  final List<Filme> filmes = Database.getFilmes();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('CineFlutter'),
+          backgroundColor: const Color.fromARGB(255, 192, 31, 171),
+        ),
+
+        body: ListView.builder(
+          itemCount: filmes.length,
+          itemBuilder: (context, index) {
+            final filme = filmes[index];
+
+            // Define a cor da estrela
+            Color corEstrela =
+                filme.imdb > 8.5 ? Colors.amber : Colors.grey;
+
+            return ListTile(
+              leading: const Icon(
+                Icons.movie,
+                color: Color.fromARGB(255, 165, 48, 106),
+              ),
+
+              title: Text(filme.titulo),
+
+              subtitle: Text('Ano: ${filme.anoLancamento}'),
+
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(
+                    Icons.star,
+                    color: corEstrela,
+                    size: 18,
+                  ),
+                  Text(filme.imdb.toString()),
+                ],
+              ),
+
+              onTap: () {
+                print('Clicou em: ${filme.titulo}');
+              },
+
+              // LongPress implementado
+              onLongPress: () {
+                print('Clicou LongPress em: ${filme.titulo}');
+              },
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
